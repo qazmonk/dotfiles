0d93183e619bad8ae25a169d4b77b0947e2e4170
Nate Chodosh <nchodosh@gmail.com>
2018-05-03 21:31:44 -0400

    minor fixes and additions to python and lisp modes

--- init.el
+++ init.el
@@ -45,3 +45,18 @@
     (my-tangle-config-org))
 (load "~/dotfiles/config.el")
 
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (visible-mark use-package undo-tree tuareg tagedit smex slime-company paredit neotree matlab-mode magit lua-mode lorem-ipsum imenu-anywhere image+ ido-ubiquitous highlight-parentheses help-fns+ god-mode gitignore-mode font-lock-studio electric-spacing electric-operator electric-case diminish crontab-mode company-auctex cmake-mode beacon ace-jump-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
