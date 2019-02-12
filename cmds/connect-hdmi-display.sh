31c7439ed421946e18d5b2eb045bda104e015faf
Nate Chodosh <nchodosh@andrew.cmu.edu>
2019-02-12 12:12:42 -0500

    Updated name of HDMI port

--- cmds/connect-hdmi-display.sh
+++ cmds/connect-hdmi-display.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-xrandr --output HDMI2 --auto
+xrandr --output HDMI-2 --auto
 if [ "$1" = "left" ]
 then
-    xrandr --output HDMI2 --left-of eDP1
+    xrandr --output HDMI-2 --left-of eDP-1
 elif [ "$1" = "right" ]
 then
-    xrandr --output HDMI2 --right-of eDP1
+    xrandr --output HDMI-2 --right-of eDP-1
 elif [ "$1" = "disconnect" ]
 then
-    xrandr --output HDMI2 --off
+    xrandr --output HDMI-2 --off
 else
     echo "Unkown argument"
     echo "Usage: connect-hdmi-display [left | right | disconnect]"
