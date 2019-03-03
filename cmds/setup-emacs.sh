26d69fa66380b136e6f7bd39c02132131dbd1389
Nate Chodosh <qazmonk@gmail.com>
2019-03-02 23:37:24 -0500

    added make emacs script

---
+++ cmds/setup-emacs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt install autoconf automake make
+sudo aptitude install libpng-dev zlib1g-dev
+sudo aptitude install libpoppler-glib-dev
+sudo aptitude install libpoppler-private-dev
+sudo aptitude install imagemagick
+
+cd ~/packages/pdf-tools/
+
+make install-server-deps
+make -s
+
+
+cd -
+
+sudo apt install elpa-counsel elpa-ivy-hydra
