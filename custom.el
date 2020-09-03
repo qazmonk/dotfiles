e48fdfc2d2d7d2bd1a1c7d03f885a8b74b91844c
Nate Chodosh <nchodosh@andrew.cmu.edu>
2020-09-03 12:11:08 -0400

    finished rewrite

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (dired-subtree zenburn-theme yasnippet-snippets xterm-color vterm visible-mark use-package undo-tree tuareg tagedit sublime-themes smex slime-company pyenv-mode org-ref neotree names matlab-mode magit-popup magit lua-mode lsp-ui lorem-ipsum ivy-yasnippet ivy-hydra interleave imenu-anywhere image+ ido-ubiquitous highlight-parentheses highlight-indent-guides help-fns+ gscholar-bibtex god-mode gitignore-mode ghub font-lock-studio flycheck-package find-file-in-project exec-path-from-shell eval-in-repl elpy electric-spacing electric-operator electric-case diminish crontab-mode counsel company-lsp company-auctex cmake-mode beacon backup-each-save ace-jump-mode))))
+    (dired-subtree zenburn-theme yasnippet-snippets xterm-color vterm visible-mark use-package undo-tree tuareg tagedit sublime-themes smex slime-company pyenv-mode org-ref neotree names matlab-mode magit-popup magit lua-mode lsp-ui lorem-ipsum ivy-yasnippet ivy-hydra interleave imenu-anywhere image+ ido-ubiquitous highlight-parentheses highlight-indent-guides help-fns+ gscholar-bibtex god-mode gitignore-mode ghub font-lock-studio flycheck-package find-file-in-project exec-path-from-shell eval-in-repl elpy electric-spacing electric-operator electric-case diminish crontab-mode counsel company-auctex cmake-mode beacon backup-each-save ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
