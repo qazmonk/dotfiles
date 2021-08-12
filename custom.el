977f09cd1be560237eecdea40f327829344c654c
Nate Chodosh <nchodosh@andrew.cmu.edu>
2021-08-11 23:37:11 -0400

    added selectrum, fixed agenda stuff

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
  '(display-line-numbers t)
  '(display-line-numbers-major-tick 200)
  '(package-selected-packages
-   '(org-journal lsp-python-ms lsp-pyright yaml-mode lsp-jedi dash lsp-mode t persistent-scratch chess picpocket lsp-ui dired-subtree zenburn-theme yasnippet-snippets xterm-color vterm visible-mark use-package undo-tree tuareg tagedit sublime-themes smex slime-company pyenv-mode org-ref neotree names matlab-mode magit-popup magit lua-mode lorem-ipsum ivy-yasnippet ivy-hydra interleave imenu-anywhere image+ ido-ubiquitous highlight-parentheses highlight-indent-guides help-fns+ gscholar-bibtex god-mode gitignore-mode ghub font-lock-studio flycheck-package find-file-in-project exec-path-from-shell eval-in-repl elpy electric-spacing electric-operator electric-case diminish crontab-mode counsel company-auctex cmake-mode beacon backup-each-save ace-jump-mode))
+   '(selectrum-prescient selectrum-precient selectrum esup org-journal lsp-python-ms lsp-pyright yaml-mode lsp-jedi dash lsp-mode t persistent-scratch chess picpocket lsp-ui dired-subtree zenburn-theme yasnippet-snippets xterm-color vterm visible-mark use-package undo-tree tuareg tagedit sublime-themes smex slime-company pyenv-mode org-ref neotree names matlab-mode magit-popup magit lua-mode lorem-ipsum ivy-yasnippet ivy-hydra interleave imenu-anywhere image+ ido-ubiquitous highlight-parentheses highlight-indent-guides help-fns+ gscholar-bibtex god-mode gitignore-mode ghub font-lock-studio flycheck-package find-file-in-project exec-path-from-shell eval-in-repl elpy electric-spacing electric-operator electric-case diminish crontab-mode counsel company-auctex cmake-mode beacon backup-each-save ace-jump-mode))
  '(safe-local-variable-values
    '((unison-sync-remote-host . "autobot")
      (unison-sync-remote-path . "/home/nchodosh")
@@ -20,4 +20,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:background nil)))))
