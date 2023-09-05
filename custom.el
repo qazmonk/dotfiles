fa3bd04a67f35b1fb78be27f63575811aa021b22
Nate Chodosh <nchodosh@andrew.cmu.edu>
2023-09-05 14:48:29 -0400

    unfill and removing moody

--- custom.el
+++ custom.el
@@ -3,11 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(package-selected-packages
-   '(python-isort python-black rust-mode eglot-grammarly flycheck-grammarly projectile diminish zenburn-theme yasnippet-snippets use-package undo-tree selectrum-prescient moody mode-line-bell marginalia imenu-anywhere hydra highlight-parentheses f exec-path-from-shell dashboard ctrlf corfu cape avy)))
+   '(csv-mode python-isort python-black rust-mode eglot-grammarly flycheck-grammarly projectile diminish zenburn-theme yasnippet-snippets use-package undo-tree selectrum-prescient moody mode-line-bell marginalia imenu-anywhere hydra highlight-parentheses f exec-path-from-shell dashboard ctrlf corfu cape avy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mode-line ((t (:background "#37474F" :foreground "#FFFFFF")))))
