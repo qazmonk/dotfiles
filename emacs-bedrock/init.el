3f387c1dd4c54f681b278e6302c06fa96938f419
Nate Chodosh <nchodosh@gmail.com>
2024-01-25 12:38:20 -0500

    setup updates

--- emacs-bedrock/init.el
+++ emacs-bedrock/init.el
@@ -181,6 +181,9 @@ If the new path's directories does not exist, create them."
 (let ((hl-line-hooks '(text-mode-hook prog-mode-hook)))
   (mapc (lambda (hook) (add-hook hook 'hl-line-mode)) hl-line-hooks))
 
+;; remove tool bar
+(tool-bar-mode -1)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Dired
@@ -266,7 +269,7 @@ If the new path's directories does not exist, create them."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(vundo org-roam cape pyenv-mode vterm which-key)))
+ '(package-selected-packages '(rg vundo org-roam cape pyenv-mode vterm which-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
